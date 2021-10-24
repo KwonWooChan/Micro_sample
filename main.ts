882d74d83900ddc502910a9fc8e9618ebba826a6
KwonWooChan <32948824+KwonWooChan@users.noreply.github.com>
2021-10-24 18:00:53 +0900

    move the counter to a button press

--- main.ts
+++ main.ts
@@ -1,4 +1,9 @@
-let counter = 0
-basic.forever(function () {
+input.onButtonPressed(Button.A, function () {
     counter += 1
+    basic.showNumber(counter)
+})
+let counter = 0
+counter = 0
+basic.forever(function () {
+	
 })
